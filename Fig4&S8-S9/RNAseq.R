f1a393196bc6b1f01ca51c0a149d7d712497cf96
Chu Tianhao <109452811+Cthfudan@users.noreply.github.com>
2025-06-08 20:00:56 +0800

    Add files via upload

--- Fig4&S8-S9/RNAseq.R
+++ Fig4&S8-S9/RNAseq.R
@@ -116,3 +116,170 @@ for(i in seq_along(pathway)){
 
 counts_normalized = counts(dds, normalized = T)
 GSVA_score = GSVA::gsva(expr = counts_normalized, gset.idx.list = pathway_score, parallel.sz = 10)
+
+res_ox_CM_table = res_ox_CM_table %>% 
+  arrange(desc(log2FoldChange))
+
+res_sig = res_ox_CM_table %>% 
+  filter(log2FoldChange > 1)
+
+res_sig = rownames(res_sig)
+
+# subset analysis of tumor-associated myeloid cells
+# filter out normal samples and run clustering/annotation on myeloid cells in tumor samples only
+
+# load libraries
+library(Seurat)
+library(tidyverse)
+library(harmony)
+library(MAST)
+library(AnnoProbe)
+library(ggpubr)
+library(ggrepel)
+library(sceasy)
+library(ggsci)
+'%notin%' = Negate('%in%')
+
+# subset the myeloid cells
+seu_anno = convertFormat('project/mice_landscape/sc_mice_annotation.h5ad', from = 'anndata', to = 'seurat', main_layer = 'counts')
+
+seu_myeloid <- subset(seu_anno, annotation %in% c('Monocyte-linage') & condition == "PT")
+
+# standard pipline
+seu_myeloid <- seu_myeloid %>% 
+  NormalizeData() %>% 
+  FindVariableFeatures() %>% 
+  ScaleData(vars.to.regress = c('mt_ratio')) %>% 
+  RunPCA()
+
+seu_myeloid <- RunHarmony(seu_myeloid, group.by.vars = "sample_id", plot_convergence = T, max.iter.harmony = 30)
+
+seu_myeloid <- RunUMAP(seu_myeloid, reduction = "harmony", dims = 1:30) %>% 
+  FindNeighbors(reduction = "harmony", dims = 1:30) %>% 
+  FindClusters(res = c(0.8))
+
+DimPlot(seu_myeloid, label = T, group.by = "RNA_snn_res.0.8") + 
+  scale_color_igv()
+
+Idents(seu_myeloid) <- "RNA_snn_res.0.8"
+
+# Find markers
+markers_myeloid <- FindAllMarkers(seu_myeloid, test.use = "wilcox", logfc.threshold = 0.5, min.pct = 0.25, only.pos = T)
+marker2 = FindMarkers(seu_myeloid, ident.1 = 0, ident.2 = 2, logfc.threshold = 0.25)
+marker2 = marker2 %>% 
+  arrange(avg_log2FC)
+markers_myeloid = markers_myeloid %>% 
+  mutate(diff.pct = pct.1 - pct.2)
+markers_top20 <- markers_myeloid %>% 
+  filter(p_val_adj <= 0.05) %>% 
+  group_by(cluster) %>% 
+  slice_max(avg_log2FC, n = 30)
+
+# Annotation 
+
+# Annotation
+
+seu_myeanno <- RenameIdents(seu_myeloid, "0" = "RTM-like TAM",  # CD5L+ VCAM1+
+                             "1" = "Inflam-TAM", 
+                             "2" = "Cycling cells", 
+                             "3" = "RTM-like TAM", 
+                             "4" = "Folr2+ TAM",
+                             "5" = "cDC2", # Clec10a+ DC
+                             "6" = "Trem2+ Spp1+ LAM", 
+                             "7" = "pDC", # Siglech+ Ccr9+
+                             "8" = "cDC1", # Xcr1+ Wdfy4+ Irf8+
+                             "10" = "cDC1")
+DimPlot(seu_myeanno)
+
+# GSEA analysis on scRNA-seq datasets
+Idents(seu) = seu$annotation2
+markers = FindMarkers(seu, ident.1 = "Trem2+ Spp1+ LAM", test.use = "MAST", logfc.threshold = .01)
+saveRDS(markers, file = "markers_Trem2+tam.rds")
+markers = markers %>% 
+  arrange(desc(avg_log2FC))
+
+gene_list = markers$avg_log2FC
+
+names(gene_list) = rownames(markers)
+
+## GSEA analysis
+
+geneset = data.frame(term = "oxLDL-up signature", gene = res_sig)
+
+gene_list = sort(gene_list, decreasing = TRUE)
+
+gene_list = gene_list[is.finite(gene_list)]
+
+gsea_result = GSEA(geneList = gene_list, TERM2GENE = geneset, pvalueCutoff = 1, pAdjustMethod = 'BH', nPermSimple = 10000, eps = 0)
+
+gsea_table = as.data.frame(gsea_result)
+
+gseaplot2(gsea_result, geneSetID = rownames(gsea_table)[1], 
+          title = '', 
+          color = '#44995F', 
+          pvalue_table = F, 
+          ES_geom = 'line', 
+          base_size = 14, 
+          rel_heights = c(1, .2, .4)
+)
+
+gsea_table_final = gsea_table # used to combine multiple gsea result
+
+## GSEA analysis on multiple celltype
+
+markers_rtm = FindMarkers(seu, ident.1 = "RTM-like TAM", test.use = "MAST", logfc.threshold = .01)
+markers_folr2 = FindMarkers(seu, ident.1 = "Folr2+ TAM", test.use = "MAST", logfc.threshold = .01)
+markers_inflam = FindMarkers(seu, ident.1 = "Inflam-TAM", test.use = "MAST", logfc.threshold = .01)
+markers_cycling = FindMarkers(seu, ident.1 = "Cycling cells", test.use = "MAST", logfc.threshold = .01)
+
+markers_list = list(markers_rtm, markers_folr2, markers_inflam, markers_cycling)
+
+for(i in markers_list){
+  i = i %>% 
+    arrange(desc(avg_log2FC))
+  gene_list = i$avg_log2FC
+  names(gene_list) = rownames(i)
+  gene_list = sort(gene_list, decreasing = TRUE)
+  gene_list = gene_list[is.finite(gene_list)]
+  gsea_result = GSEA(geneList = gene_list, TERM2GENE = geneset, pvalueCutoff = 1, pAdjustMethod = 'BH', nPermSimple = 10000, eps = 0)
+  gsea_table = as.data.frame(gsea_result)
+  gsea_table_final = rbind(gsea_table_final, gsea_table)
+}
+rownames(gsea_table_final) = c("Trem2+ LAM", "RTM-like TAM", "Folr2+ TAM", "Inflam-TAM", "Cycling TAM")
+gsea_table_final = gsea_table_final %>% 
+  rownames_to_column(var = "celltype")
+
+gsea_table_final = gsea_table_final %>% 
+  mutate(logfdr = -log10(p.adjust))
+
+gsea_table_final = gsea_table_final %>% 
+  arrange(desc(NES))
+
+gsea_table_final$celltype = factor(gsea_table_final$celltype, levels = rev(unique(gsea_table_final$celltype)))
+  
+ggplot(gsea_table_final, aes(x = NES, y = celltype, size = logfdr)) + 
+  geom_point() + 
+  coord_cartesian(xlim = c(-3, 3)) +
+  ggpubr::theme_classic2() + 
+  geom_vline(xintercept = 0, linetype = 2)
+
+# heatmap 
+
+library(pheatmap)
+
+gene_int = enrich_gene
+exp_mat = counts(dds, normalize = T)
+library(RColorBrewer)
+mycol = colorRampPalette(brewer.pal(n = 6, name = "RdBu"))(100)
+
+gene_to_label = c("Edn1", "Fabp3", "Ptges", "Furin", "Hrh2", "Ptgs2", "Il10", "Src", "Bnip3", "Tnfaip3", "Rcan1", "Cxcl9", "Ccl22", "Ccl5", "Il1rn")
+
+row_label = rep("", length = length(gene_int))
+
+names(row_label) = gene_int
+
+row_label[gene_to_label] = gene_to_label
+
+p = pheatmap(exp_mat[gene_int, ], cluster_rows=T, 
+             cluster_cols=F, scale = "row", border_color = NA, color = rev(mycol), labels_row = row_label)
+
